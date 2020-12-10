712085ed0b06af565fdaea329e37df7213ba9296
Kaja <kaja.osojnik@vigoshop.si>
2020-12-10 13:11:46 +0100

    PRepared for deployment

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
-const connectDB = require('./config/db')
+const connectDB = require('./config/db');
+const path = require('path');
 
 const app = express();
 
@@ -10,15 +11,21 @@ connectDB();
 // init middleWare to be able to send body requests
 app.use(express.json({extended:false}))
 
-// get request in Postman
-app.get('/', (req, res) => res.send('API running for mudra'))
-
 
 // define routes
 app.use('/api/users', require('./routes/api/users'));
 app.use('/api/auth', require('./routes/api/auth'));
 app.use('/api/timers', require('./routes/api/timers'));
 
+// Serve static asets in production
+if(process.env.NODE_ENV === 'production') {
+    // Set static folder
+    app.use(express.static('client/build'));
+
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    })
+}
 
 // port either PORT on production or 7000 locally
 const PORT = process.env.PORT || 7000;
